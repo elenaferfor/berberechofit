dba3c446716d3e3b260ce3b3fd6c1373786a0591
Ismael P. Blanquer <ismael.pblanquer@gmail.com>
2025-10-18 14:06:02 +0200

    Add user

--- fitapp/models.py
+++ fitapp/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=50)
+    email = models.EmailField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
